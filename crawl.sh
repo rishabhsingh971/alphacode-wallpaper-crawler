b26f4973feb10719e32ff0cfe695992e6a5fe60d
rishabhsingh971 <rishabhsingh971@gmail.com>
2019-12-18 03:31:49 +0530

    crawl all pages

--- crawl.sh
+++ crawl.sh
@@ -21,20 +21,26 @@ download_dir_path='downloads'
 # make download directory if not exists
 mkdir -p "$download_dir_path"
 
-# source html file path
-src_file_path="$download_dir_path/$search_string.html"
-
-# download source html
-download "https://wall.alphacoders.com/search.php?search=$search_string" "$src_file_path"
+page_url="https://wall.alphacoders.com/search.php?search=$search_string"
+current_page=1
 
 # set internal field separator as newline
 IFS=$'\n'
 set -f
-# extract urls from src html file
-urls=$(cat "$src_file_path" | sed -rn 's/.*(https:\/\/images[0-9]+\.alphacoders.com\/[0-9]+\/)thumb-[0-9]+-([0-9]+.jpg).*/\1\2/p')
 
-# download each url
-echo $urls | while read url; do
-    file_path="$download_dir_path/${url##*/}"
-    download "$url" "$file_path"
+while [ ! -z "$page_url" ]; do
+    # current page html file path
+    src_file_path="$download_dir_path/$search_string.$current_page.html"
+    # download current page html
+    download "$page_url" "$src_file_path"
+    # extract urls from src html file
+    urls=$(cat "$src_file_path" | sed -rn 's/.*(https:\/\/images[0-9]+\.alphacoders.com\/[0-9]+\/)thumb-[0-9]+-([0-9]+.jpg).*/\1\2/p')
+
+    # download each url
+    echo $urls | while read url; do
+        file_path="$download_dir_path/${url##*/}"
+        download "$url" "$file_path"
+    done
+    current_page=$((current_page + 1))
+    page_url=$(cat "$src_file_path" | sed -rn 's/.*link rel="next" href="(.*&)amp;(page=[0-9]+)".*/\1\2/p')
 done
