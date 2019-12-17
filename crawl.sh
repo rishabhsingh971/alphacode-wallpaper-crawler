6fd86bed1348d1e1e8cf5f1c87158ffb9ca698c7
rishabhsingh971 <rishabhsingh971@gmail.com>
2019-12-18 04:29:25 +0530

    make a function to download page

--- crawl.sh
+++ crawl.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # check arguments
 if [ -z "$1" ]; then
     echo "Please provide search string"
@@ -21,29 +22,30 @@ download_dir_path='downloads'
 # make download directory if not exists
 mkdir -p "$download_dir_path"
 
-current_page=1
-page_url="https://wall.alphacoders.com/search.php?search=$search_string&page=$current_page"
-
-# set internal field separator as newline
-old_ifs="$IFS"
-IFS=$'\n'
-
-while [ ! -z "$page_url" ]; do
-    # current page html file path
-    src_file_path="$download_dir_path/$search_string.$current_page.html"
-    # download current page html
+download_page() {
+    # page number
+    page_number=$1
+    # page url
+    page_url="https://wall.alphacoders.com/search.php?search=$search_string&page=$page_number"
+    # page html file path
+    src_file_path="$download_dir_path/$search_string.$page_number.html"
+    # download page html
     download "$page_url" "$src_file_path"
     # extract urls from src html file
     urls=$(cat "$src_file_path" | sed -rn 's/.*(https:\/\/images[0-9]+\.alphacoders.com\/[0-9]+\/)thumb-[0-9]+-([0-9]+.jpg).*/\1\2/p')
 
     # download each url
-    echo $urls | while read url; do
+    for url in $urls; do
         file_path="$download_dir_path/${url##*/}"
         download "$url" "$file_path"
     done
-    current_page=$((current_page + 1))
-    page_url=$(cat "$src_file_path" | sed -rn 's/.*link rel="next" href="(.*&)amp;(page=[0-9]+)".*/\1\2/p')
-done
+}
 
-# reset internal field separator
-IFS="$old_ifs"
+start_page_number=1
+src_file_path=''
+download_page "$start_page_number"
+last_page_number=$(cat "$src_file_path" | sed -rn 's/.*<a title="Last Page \(([0-9]+)\).*/\1/p')
+
+for ((i = $start_page_number + 1; i < $last_page_number; ++i)); do
+    download_page "$i"
+done
