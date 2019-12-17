7909fccacf02d38b0d5679754524b485aee13f78
rishabhsingh971 <rishabhsingh971@gmail.com>
2019-12-18 00:46:25 +0530

    dont download if src file exists

--- crawl.sh
+++ crawl.sh
@@ -1,5 +1,11 @@
-curl "https://wall.alphacoders.com/search.php?search=Dark+Knight" -o 'src.html'
-cat src.html | sed -rn 's/.*(https:\/\/images[0-9]+\.alphacoders.com\/[0-9]+\/)thumb-[0-9]+-([0-9]+.jpg).*/\1\2/p' >input
+src_file_path='src.html'
+if [ -f "$src_file_path" ]; then
+    echo "$src_file_path exists"
+else
+    echo "Fetch $src_file_path"
+    curl "https://wall.alphacoders.com/search.php?search=Dark+Knight" -o "$src_file_path"
+fi
+cat "$src_file_path" | sed -rn 's/.*(https:\/\/images[0-9]+\.alphacoders.com\/[0-9]+\/)thumb-[0-9]+-([0-9]+.jpg).*/\1\2/p' >input
 IFS=$'\n'
 set -f
 foo=($(<input))
